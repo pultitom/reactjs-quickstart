8bb1a0c0856b0cd61707533fe1d1348f4c749a44
pultitom <t.pultinevicius@gmail.com>
2016-07-31 16:25:38 +0300

    added webpack config, typings, tsconfig and Hello component

--- src/ReactJsQuickstart/Startup.cs
+++ src/ReactJsQuickstart/Startup.cs
@@ -1,8 +1,6 @@
-using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
